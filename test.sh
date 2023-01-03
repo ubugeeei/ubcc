1af041f688ff64cc941fb7c670b8f945f18c2a1a
Ubugeeei <ubuge1122@gamil.com>
2023-01-03 21:29:37 +0900

    feat: test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,20 @@
-echo test
+#!/bin/bash
+assert() {
+  expected="$1"
+  input="$2"
+
+  cargo run "$input" > tmp.s
+  cc -o tmp tmp.s
+  ./tmp
+  actual="$?"
+
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+assert 0 0
+assert 42 42
