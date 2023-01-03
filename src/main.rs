259d100b7ded0abfe0b41236f289a72be784d79e
Ubugeeei <ubuge1122@gamil.com>
2023-01-03 21:27:30 +0900

    feat: compile return number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let argv = std::env::args().collect::<Vec<String>>();
+    if argv.len() != 2 {
+        eprintln!("Invalid number of arguments");
+        std::process::exit(1);
+    }
+
+    print!(".intel_syntax noprefix\n");
+    print!(".globl main\n");
+    print!("main:\n");
+    print!("  mov rax, {}\n", argv[1]);
+    print!("  ret\n");
 }
