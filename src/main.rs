ee78f5ad0486a2a5b4e593ca7c27dcca02ea3ab0
Ubugeeei <ubuge1122@gamil.com>
2023-01-09 01:35:56 +0900

    feat: use codegen

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-use crate::lex::Token;
-use std::io::{BufWriter, Write};
-
 mod ast;
 mod codegen;
 mod lex;
@@ -11,38 +8,13 @@ fn main() {
     if argv.len() != 2 {
         panic!("Invalid number of arguments");
     }
+    let input = argv[1].clone();
 
-    let mut lexer = lex::Lexer::new(argv[1].clone());
-
-    let out = std::io::stdout();
-    let mut out = BufWriter::new(out.lock());
-
-    write!(out, ".intel_syntax noprefix\n").unwrap();
-    write!(out, ".global main\n").unwrap();
-    write!(out, "main:\n").unwrap();
-
-    let mut current_token = lexer.next();
-    let Token::Integer(int) = current_token else {
-        panic!("Invalid token: {:?}", current_token);
-    };
-    write!(out, "  mov rax, {}\n", int).unwrap();
-
-    current_token = lexer.next();
-    while current_token != Token::Eof {
-        let op = match current_token {
-            Token::Plus => "add",
-            Token::Minus => "sub",
-            _ => {
-                panic!("Invalid token: {:?}", current_token);
-            }
-        };
-        current_token = lexer.next();
-        let Token::Integer(int) = current_token else {
-            panic!("Invalid token: {:?}", current_token);
-        };
-        let _ = write!(out, "  {} rax, {}\n", op, int);
-        current_token = lexer.next();
-    }
-
-    let _ = write!(out, "  ret\n");
+    println!(".intel_syntax noprefix");
+    println!(".global main");
+    println!("main:");
+    let ast = parse::parse(input);
+    codegen::gen(ast);
+    println!("  pop rax");
+    println!("  ret");
 }
