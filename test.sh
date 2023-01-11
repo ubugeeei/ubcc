f730c935ed9ba3164371448d05c5c5fe0a772bbf
Ubugeeei <ubuge1122@gamil.com>
2023-01-11 09:15:23 +0900

    test: function and vars

--- test.sh
+++ test.sh
@@ -44,3 +44,16 @@ assert 0 '1>2'
 assert 1 '1>=0'
 assert 1 '1>=1'
 assert 0 '1>=2'
+
+assert 55 '
+  sum(m, n) {
+    acc = 0;
+    for (i = m; i <= n; i = i + 1)
+      acc = acc + i;
+    return acc;
+  }
+
+  main() {
+    return sum(1, 10); // 55を返す
+  }
+'
