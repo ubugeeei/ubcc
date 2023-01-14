8b69531d59cb6d215ebf44d4c4a5b6e83768b34b
Ubugeeei <ubuge1122@gamil.com>
2023-01-14 21:34:37 +0900

    tweak: test case

--- test.sh
+++ test.sh
@@ -45,15 +45,15 @@ assert 1 '1>=0'
 assert 1 '1>=1'
 assert 0 '1>=2'
 
-assert 55 '
-  sum(m, n) {
-    acc = 0;
-    for (i = m; i <= n; i = i + 1)
-      acc = acc + i;
-    return acc;
-  }
+# assert 55 '
+#   sum(m, n) {
+#     acc = 0;
+#     for (i = m; i <= n; i = i + 1)
+#       acc = acc + i;
+#     return acc;
+#   }
 
-  main() {
-    return sum(1, 10); // 55を返す
-  }
-'
+#   main() {
+#     return sum(1, 10); // 55を返す
+#   }
+# '
