dfbae6176a1334f9a904c3036e0128c0d8bbd886
Ubugeeei <ubuge1122@gamil.com>
2023-01-14 21:44:45 +0900

    refactor

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,71 +1,86 @@
 use crate::ast::{BinaryOperator, Expression, Program, Statement};
 
+// entry
 pub(crate) fn gen(node: Program) {
-    for stmt in node.statements {
-        gen_stmt(stmt);
-    }
-
+    let codegen = CodeGenerator::new(node);
+    codegen.gen();
 }
 
-fn gen_stmt(node: Statement) {
-    match node {
-        Statement::Expression(expr) => {
-            gen_expr(expr);
-        }
+struct CodeGenerator {
+    ast: Program,
+}
+impl CodeGenerator {
+    fn new(ast: Program) -> Self {
+        Self { ast }
     }
 }
-
-fn gen_expr(node: Expression) {
-    match node {
-        Expression::Integer(int) => {
-            println!("  push {}", int);
+impl CodeGenerator {
+    fn gen(&self) {
+        for stmt in self.ast.statements.iter() {
+            self.gen_stmt(stmt);
         }
-        Expression::Binary(bin) => {
-            gen_expr(*bin.lhs);
-            gen_expr(*bin.rhs);
-            println!("  pop rdi");
-            println!("  pop rax");
-            match bin.op {
-                BinaryOperator::Plus => {
-                    println!("  add rax, rdi");
-                }
-                BinaryOperator::Minus => {
-                    println!("  sub rax, rdi");
-                }
-                BinaryOperator::Asterisk => {
-                    println!("  imul rax, rdi");
-                }
-                BinaryOperator::Slash => {
-                    println!("  cqo");
-                    println!("  idiv rdi");
-                }
-                BinaryOperator::Lt => {
-                    println!("  cmp rax, rdi");
-                    println!("  setl al");
-                    println!("  movzb rax, al");
-                }
-                BinaryOperator::LtEq => {
-                    println!("  cmp rax, rdi");
-                    println!("  setle al");
-                    println!("  movzb rax, al");
-                }
-                BinaryOperator::Eq => {
-                    println!("  cmp rax, rdi");
-                    println!("  sete al");
-                    println!("  movzb rax, al");
-                }
-                BinaryOperator::NotEq => {
-                    println!("  cmp rax, rdi");
-                    println!("  setne al");
-                    println!("  movzb rax, al");
-                } // _ => {
-                  //     panic!("Invalid binary operator: {:?}", bin.op);
-                  // }
+    }
+
+    fn gen_stmt(&self, node: &Statement) {
+        match node {
+            Statement::Expression(expr) => {
+                self.gen_expr(expr);
             }
-            println!("  push rax");
         }
-        _ => {
-            panic!("Invalid node: {:?}", node);
+    }
+
+    fn gen_expr(&self, node: &Expression) {
+        match node {
+            Expression::Integer(int) => {
+                println!("  push {}", int);
+            }
+            Expression::Binary(bin) => {
+                self.gen_expr(&*bin.lhs);
+                self.gen_expr(&*bin.rhs);
+                println!("  pop rdi");
+                println!("  pop rax");
+                match bin.op {
+                    BinaryOperator::Plus => {
+                        println!("  add rax, rdi");
+                    }
+                    BinaryOperator::Minus => {
+                        println!("  sub rax, rdi");
+                    }
+                    BinaryOperator::Asterisk => {
+                        println!("  imul rax, rdi");
+                    }
+                    BinaryOperator::Slash => {
+                        println!("  cqo");
+                        println!("  idiv rdi");
+                    }
+                    BinaryOperator::Lt => {
+                        println!("  cmp rax, rdi");
+                        println!("  setl al");
+                        println!("  movzb rax, al");
+                    }
+                    BinaryOperator::LtEq => {
+                        println!("  cmp rax, rdi");
+                        println!("  setle al");
+                        println!("  movzb rax, al");
+                    }
+                    BinaryOperator::Eq => {
+                        println!("  cmp rax, rdi");
+                        println!("  sete al");
+                        println!("  movzb rax, al");
+                    }
+                    BinaryOperator::NotEq => {
+                        println!("  cmp rax, rdi");
+                        println!("  setne al");
+                        println!("  movzb rax, al");
+                    } // _ => {
+                      //     panic!("Invalid binary operator: {:?}", bin.op);
+                      // }
+                }
+                println!("  push rax");
+            }
+            _ => {
+                panic!("Invalid node: {:?}", node);
+            }
         }
     }
 }
