189ffbf3f71abd1518858a771b802a36b62be48b
Ubugeeei <ubuge1122@gamil.com>
2023-01-15 20:53:34 +0900

    fix: test

--- test.sh
+++ test.sh
@@ -17,33 +17,33 @@ assert() {
   fi
 }
 
-assert 0 0
-assert 42 42
+assert 0 "0;"
+assert 42 "42;"
 
-assert 21 "5 + 20 - 4"
-assert 47 '5 + 6 * 7'
+assert 21 "5 + 20 - 4;"
+assert 47 '5 + 6 * 7;'
 
-assert 15 '5 * (9 - 6)'
-assert 4 '(3 + 5) / 2'
+assert 15 '5 * (9 - 6);'
+assert 4 '(3 + 5) / 2;'
 
-assert 0 '0 == 1'
-assert 1 '42 == 42'
-assert 1 '0 != 1'
-assert 0 '42 != 42'
+assert 0 '0 == 1;'
+assert 1 '42 == 42;'
+assert 1 '0 != 1;'
+assert 0 '42 != 42;'
 
-assert 1 '0 < 1'
-assert 0 '1 < 1'
-assert 0 '2 < 1'
-assert 1 '0 <= 1'
-assert 1 '1 <= 1'
-assert 0 '2 <= 1'
+assert 1 '0 < 1;'
+assert 0 '1 < 1;'
+assert 0 '2 < 1;'
+assert 1 '0 <= 1;'
+assert 1 '1 <= 1;'
+assert 0 '2 <= 1;'
 
-assert 1 '1 > 0'
-assert 0 '1 > 1'
-assert 0 '1 > 2'
-assert 1 '1 >= 0'
-assert 1 '1 >= 1'
-assert 0 '1 >= 2'
+assert 1 '1 > 0;'
+assert 0 '1 > 1;'
+assert 0 '1 > 2;'
+assert 1 '1 >= 0;'
+assert 1 '1 >= 1;'
+assert 0 '1 >= 2;'
 
 assert 4 'a = 2; a + 2;'
 assert 7 "\
